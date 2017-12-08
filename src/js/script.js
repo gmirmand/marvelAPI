03fee344c9acd05b4adceddd0af83dc01b027818
GaetanMSI <gmirmand@gmail.com>
2017-12-08 14:34:04 +0100

    style & limit5 add

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,11 @@ $(document).ready(function () {
         function log(message) {
             $("#result").empty().append(message);
         }
-
         $search.autocomplete({
             source: function (request, response) {
                 $.ajax({
                     type: "GET",
-                    url: "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + $search.val() + "&apikey=a46dd2d60494b578a99e8c63c3c76451",
+                    url: "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" + $search.val() + "&apikey=a46dd2d60494b578a99e8c63c3c76451&limit=5",
                     dataType: "json",
                     success: function (data) {
                         results = data.data.results;
@@ -33,7 +32,6 @@ $(document).ready(function () {
                     if (results[i].name === ui.item.value) {
                         if (!results[i].description)
                             results[i].description = "Désolé, il n'y a pas de description.";
-
                         //Text and desc
                         log("<h2 class='script'><span>" + results[i].name + "</span></h2>" +
                             "<p> " + results[i].description + "</p>");
@@ -43,7 +41,6 @@ $(document).ready(function () {
                         //Comics
                         if (results[i].comics.items[0]) {
                             $('.comics').empty().removeClass('hide').append("<p class='more'>" + results[i].name + " apparait dans " + results[i].comics.available + " comics.<br>dont...</p>");
-
                         }
                         else
                             $('.comics').addClass('hide');
@@ -51,8 +48,6 @@ $(document).ready(function () {
                         $.each(results[i].comics.items, function (j) {
                             $('.comics').append("<p class='txtshadow'><span>" + results[i].comics.items[j].name + "</span></p>");
                         });
-
-
                     }
                 });
             }
